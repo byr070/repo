e78323752342469cf6db13dd33135b7470034deb
byr070 <byr_070@hotmail.com>
2014-07-21 13:37:49 +0000

    Con la base

---
+++ application/views/mivista.php
@@ -0,0 +1,12 @@
+<html lang="es">
+<head>
+<title><?=$titulo?></title>
+</head>
+<body>
+<h1><?=$titulo?></h1>
+   <blockquote>
+      <p><b><?=$descripcion?></b></p>
+   </blockquote>
+   <p><?=$cuerpo?></p>
+</body>
+</html> 
