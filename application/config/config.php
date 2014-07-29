cba9b4c17e3ada1e6d4950fee1bcf079c7c91c7b
byr070 <byr_070@hotmail.com>
2014-07-29 22:44:56 +0000

    inicio

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8234/webapp';
+$config['base_url']	= 'http://localhost/webapp';
 
 /*
 |--------------------------------------------------------------------------
