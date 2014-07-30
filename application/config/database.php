0376107e54acb4eb74b82133ee4bef3d32f7c974
DANIEL TAPIA <danieltapiamoreno@gmail.com>
2014-07-29 22:02:04 -0500

    se edito database.php

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'toor';
-$db['default']['database'] = 'db_sgth';
+$db['default']['password'] = 'root';
+$db['default']['database'] = 'tesis';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
